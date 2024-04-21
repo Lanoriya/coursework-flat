a09fff6bfe72e01d03279d93e3d6d5f3d7243303
lanoriya <Mr_Buzik@mail.ru>
2024-04-22 03:08:29 +0500

    deals without css

--- src/components/user/Profile/Deals.js
+++ src/components/user/Profile/Deals.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Deals() {
+  const [deals, setDeals] = useState([]);
+
+  useEffect(() => {
+    // Получение данных о сделках из localstorage
+    const storedDeals = JSON.parse(localStorage.getItem('deals')) || [];
+    setDeals(storedDeals);
+  }, []);
+
   return (
-    <div>Deals</div>
+    <div className='deals-container'>
+      <ul className='deals-ul'>
+        {deals.map((deal, index) => (
+          <li className='deals-li' key={index}>
+            <p>Квартира №{deal.apartment_number}</p>
+            <p>Площадь: {deal.area}м²</p>
+            <p>Этаж: {deal.floor}</p>
+            <p>Цена: {deal.price}</p>
+            <img className="flat-img" src={`http://localhost:3001/api/image/${deal.image_id}`} alt={`Apartment ${deal.apartment_number}`} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- src/components/user/Profile/Favorites.js
+++ src/components/user/Profile/Favorites.js
@@ -3,11 +3,16 @@ import React, { useState, useEffect } from 'react';
 function Favorites() {
   const [favoriteApartments, setFavoriteApartments] = useState([]);
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [deals, setDeals] = useState([]);
 
   useEffect(() => {
     // Получение данных избранных квартир из localstorage
     const storedFavorites = JSON.parse(localStorage.getItem('cartItems')) || [];
     setFavoriteApartments(storedFavorites);
+
+    // Получение сделок из localstorage
+    const storedDeals = JSON.parse(localStorage.getItem('deals')) || [];
+    setDeals(storedDeals);
   }, []);
 
   const removeFromFavorites = (index) => {
@@ -17,6 +22,29 @@ function Favorites() {
 
     // Сохраняем обновленный массив в localstorage
     localStorage.setItem('cartItems', JSON.stringify(updatedFavorites));
+  };
+
+  const addToDeal = (index) => {
+    // Получаем квартиру, которую нужно добавить в сделку
+    const apartmentToAdd = favoriteApartments[index];
+
+    // Проверяем, существует ли уже сделка на эту квартиру
+    const isAlreadyInDeals = deals.some(deal => deal.id === apartmentToAdd.id);
+    if (!isAlreadyInDeals) {
+      // Если сделки на квартиру нет, то добавляем её в массив сделок
+      const updatedDeals = [...deals, apartmentToAdd];
+      setDeals(updatedDeals);
+
+      // Сохраняем обновленный массив сделок в localstorage
+      localStorage.setItem('deals', JSON.stringify(updatedDeals));
+    } else {
+      // Если сделка уже существует, то удаляем её из сделок
+      const updatedDeals = deals.filter(deal => deal.id !== apartmentToAdd.id);
+      setDeals(updatedDeals);
+
+      // Сохраняем обновленный массив сделок в localstorage
+      localStorage.setItem('deals', JSON.stringify(updatedDeals));
+    }
   };
 
   return (
@@ -38,9 +66,11 @@ function Favorites() {
                 src={`http://localhost:3001/api/image/${apartment.image_id}`} 
                 alt={`Apartment ${apartment.apartment_number}`}
               />
-              {/* Кнопка для удаления квартиры из избранного */}
             </div>
-            <button className='favorites-remover' onClick={() => removeFromFavorites(index)}>Удалить из избранного</button>
+            <button className='favorites-btn favorites-remover' onClick={() => removeFromFavorites(index)}>Удалить из избранного</button>
+            <button className='favorites-btn favorites-deal' onClick={() => addToDeal(index)}>
+              {deals.some(deal => deal.id === apartment.id) ? 'Очистить сделки' : 'Начать сделку'}
+            </button>
           </li>
         ))}
       </ul>
