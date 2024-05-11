c6c98697f51e0c688ad6268c6d16ef406a79875f
lanoriya <Mr_Buzik@mail.ru>
2024-05-11 20:55:28 +0500

    drag and drop add

--- src/components/admin/fullReview/buildingPhotosPopup/BuildingPhotosPopup.js
+++ src/components/admin/fullReview/buildingPhotosPopup/BuildingPhotosPopup.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import Draggable from 'react-draggable';
 
 function BuildingPhotosPopup({ buildingPhotos, onClose }) {
 
   return (
-    <div className="building-photos-popup">
-      <div className="building-close-btn" onClick={onClose}></div>
-      <div className="building-photos-container">
-        {buildingPhotos.map((photo, index) => (
-          <img className='building-photo' key={index} src={'http://localhost:3001/' + photo.image_url} alt={`Photo ${index + 1}`} />
-        ))}
+    <Draggable bounds=".admin-main" handle="strong">
+      <div className="building-photos-popup">
+        <strong className='mover'/>
+        <div className="building-close-btn" onClick={onClose}></div>
+        <div className="building-photos-container">
+          {buildingPhotos.map((photo, index) => (
+            <img className='building-photo' key={index} src={'http://localhost:3001/' + photo.image_url} alt={`Photo ${index + 1}`} />
+          ))}
+        </div>
       </div>
-    </div>
+    </Draggable>
   );
 }
 
